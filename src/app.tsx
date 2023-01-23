341dcc92d87f95236672d47b5819abb39a4dc1b8
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-01-22 17:48:11 -0800

    Set up basic app

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./app.css";
+
+const App = () => {
+  return <div className="app-container"> hello! </div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
