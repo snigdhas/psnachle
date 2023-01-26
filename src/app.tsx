4f32f78cb9eb020c2509c3265908ac4243354166
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-01-25 20:48:11 -0800

    Add app routes and styling

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import Classes from "./components/Classes";
+import ContactUs from "./components/ContactUs";
+import Events from "./components/Events";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import OurStory from "./components/OurStory";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
 const App = () => {
-  return <div className="app-container"> hello! </div>;
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />} path="/">
+            <Route element={<OurStory />} path="ourstory" />
+            <Route element={<Classes />} path="classes" />
+            <Route element={<Events />} path="events" />
+            <Route element={<ContactUs />} path="contact-us" />
+            <Route element={<Home />} path="*" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
