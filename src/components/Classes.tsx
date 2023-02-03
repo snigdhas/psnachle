2b473c2891ef9f89be4a46c0f560b3b92ea86fe2
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-02-02 22:04:14 -0800

    Add images to classes page

--- src/components/Classes.tsx
+++ src/components/Classes.tsx
@@ -5,20 +5,26 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import { Helmet } from "react-helmet-async";
+import groupSession from "../img/group.webp";
+import performanceSeries from "../img/performance.webp";
+import privateSession from "../img/private.webp";
 
 const Classes = () => {
   const classes = [
     {
+      img: performanceSeries,
       title: "Performance Series",
       text: "Join an 8-week series of classes where we will work on 2-3 songs, focusing on technique and execution, and culminating in a performance amongst friends and family.",
       button: "Sign up",
     },
     {
+      img: privateSession,
       title: "Private Session (1-2 people)",
       text: "Sign up for a private session with one of our instructors. Please fill out the form with details about what styles you want to focus on and we'll tailor the session to you.",
       button: "Sign up",
     },
     {
+      img: groupSession,
       title: "Private Session (Group)",
       text: "Schedule a private session with one of our instructors. These sessions are best suited for groups looking to learn and practice for performances at events.",
       button: "Sign up",
@@ -31,10 +37,11 @@ const Classes = () => {
         <title>Classes</title>
       </Helmet>
       <Col className="d-flex flex-row">
-        {classes.map(({ title, text, button }) => (
+        {classes.map(({ img, title, text, button }) => (
           <>
             <Card className="me-4 border-1">
               {/* TODO: Add image */}
+              <Card.Img src={img} variant="top" />
               <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{text}</Card.Text>
